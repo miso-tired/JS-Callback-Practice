41a6fca4020040e097dd00a8911635dd60470ee3
Anthony <anthonylsa12@gmail.com>
2023-12-03 11:51:00 -0800

    deleting old logic removes all elements except the character, and the character no longer moves

--- index.js
+++ index.js
@@ -6,39 +6,39 @@ let direction = null;
 let x = 100;
 let y = 250;
 
-function moveCharacter() {
-    if (direction === "west") {
-        x = x - 1
-    }
-    if (direction === "east") {
-        x = x + 1
-    }
-    if (direction === "north") {
-        y = y + 1
-    }
-    if (direction === "south") {
-        y = y - 1
-    }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}
+// function moveCharacter() {
+//     if (direction === "west") {
+//         x = x - 1
+//     }
+//     if (direction === "east") {
+//         x = x + 1
+//     }
+//     if (direction === "north") {
+//         y = y + 1
+//     }
+//     if (direction === "south") {
+//         y = y - 1
+//     }
+//     character.style.left = x + 'px'
+//     character.style.bottom = y + 'px'
+// }
 
-document.addEventListener ('keydown', function(e) {
-    if (e.repeat) return;
+// document.addEventListener ('keydown', function(e) {
+//     if (e.repeat) return;
 
-    if (e.key === "ArrowLeft") {
-        direction = 'west'
-    }
-    if (e.key === "ArrowRight") {
-        direction = 'east'
-    }
-    if (e.key === "ArrowUp") {
-        direction = 'north'
-    }
-    if (e.key === "ArrowDown") {
-        direction = 'south'
-    }
-})
+//     if (e.key === "ArrowLeft") {
+//         direction = 'west'
+//     }
+//     if (e.key === "ArrowRight") {
+//         direction = 'east'
+//     }
+//     if (e.key === "ArrowUp") {
+//         direction = 'north'
+//     }
+//     if (e.key === "ArrowDown") {
+//         direction = 'south'
+//     }
+// })
 
 document.addEventListener('keyup', function(e) {
     direction = null
